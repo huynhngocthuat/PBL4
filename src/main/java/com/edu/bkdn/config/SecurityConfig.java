b47c0415714b7663143d40e1a8e3c6ad423fa6ee
= <=>
2021-11-05 22:38:44 +0700

    fix load static resources

--- src/main/java/com/edu/bkdn/config/SecurityConfig.java
+++ src/main/java/com/edu/bkdn/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -49,5 +50,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordParameter("password");
     }
 
-
+    @Override
+    public void configure(WebSecurity web){
+        web
+            .ignoring()
+            .antMatchers("/resources/**", "/static/**",
+                    "/css/**", "/js/**", "/images/**","/template/**");
+    }
 }
