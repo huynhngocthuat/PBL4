67fe6d14ca7ed1cfc55f1bb7caa53f3e36659baa
= <=>
2021-11-09 21:01:07 +0700

    error-git-checkout-branch

--- src/main/java/com/edu/bkdn/controllers/homeController.java
+++ src/main/java/com/edu/bkdn/controllers/homeController.java
@@ -1,10 +1,8 @@
 package com.edu.bkdn.controllers;
 
-import com.edu.bkdn.models.ApplicationUser;
 import com.edu.bkdn.models.User;
 import com.edu.bkdn.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Controller
-public class homeController {
+public class HomeController {
     @Autowired
     private UserService userService;
 
@@ -20,16 +18,6 @@ public class homeController {
     public String index(ModelMap modelMap){
         List<User> list = userService.findAll();
         modelMap.addAttribute("users", list);
-        return "index";
+        return "index.html";
     }
-
-    @GetMapping("/hello")
-    public String hello(ModelMap modelMap, Authentication authentication){
-        String userName = "";
-        userName = ((ApplicationUser)authentication.getPrincipal()).getUser().getEmail();
-        System.out.println("Username: " + userName);
-        modelMap.addAttribute("helloUser", userName);
-        return "hello";
-    }
-
 }
