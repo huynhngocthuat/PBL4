81f072f4776abb4034affc4cc05424e19fd7e32e
= <=>
2021-12-17 01:46:47 +0700

    change file config 2

--- src/main/java/com/edu/bkdn/config/WebSocketConfig.java
+++ src/main/java/com/edu/bkdn/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("https://pbl4-production-e5e1.up.railway.app/gkz-stomp-endpoint").withSockJS();
+        registry.addEndpoint("https://pbl4-production-e5e1.up.railway.app/gkz-stomp-endpoint/**").withSockJS();
     }
 
     @Override
