f8f3540836ee758955b58c0657f74dfaaca59f81
= <=>
2021-12-17 01:33:44 +0700

    add dependency

--- src/main/java/com/edu/bkdn/config/WebSocketConfig.java
+++ src/main/java/com/edu/bkdn/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.edu.bkdn.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -19,4 +20,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
+
+
 }
