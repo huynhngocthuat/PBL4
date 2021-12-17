7be9c3cc3fb779b2fa15456397526ce37e08ff97
= <=>
2021-12-17 17:11:12 +0700

    change file config 3

--- src/main/java/com/edu/bkdn/config/WebSocketConfig.java
+++ src/main/java/com/edu/bkdn/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.edu.bkdn.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,6 +10,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@CrossOrigin("*")
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
